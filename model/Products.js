1fc5cace097f85e6b14e0e84a3557aa0b04fa1a0
SimanyeSTS <simanyesomdaka@gmail.com>
2024-08-20 15:10:17 +0200

    single product retrieval complete

--- model/Products.js
+++ model/Products.js
@@ -21,8 +21,30 @@ class Products {
             })
         }
     }
-    
-
+    fetchProduct(req, res) {
+        try {
+            const strQry = `
+            SELECT prodID, prodImage, prodName, quantity, amount, Category, prodDescription, prodURL
+            FROM Products
+            WHERE productID = ${req.params.id};    
+            `
+            db.query(strQry, (err, result) => {
+                if (err) throw new Error('Our apologies, an issue seems to have occured whe retrieving this product. Please try again later.')
+                    res.json({
+                status: res.statusCode,
+                result: result[0]
+        })
+            })
+        } catch (e) {
+            res.json({
+                status: 404,
+                msg: e.message
+            })
+        }
+    }
+    addProduct(req, res) {
+        
+    }
 
 
 
