d24c0be6606f323e78a3e4d63ffa4bdd4b71ffbe
Tyra Parring <parringt@gmail.com>
2024-08-21 15:47:17 +0200

    worked on contact page

--- Documents/AuraArtistry/aura-artistry/src/store/index.js
+++ Documents/AuraArtistry/aura-artistry/src/store/index.js
@@ -1,14 +1,144 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
+
+const hostedData = "https://auraartistry-project.onrender.com/";
+
 
 export default createStore({
   state: {
+    products: [],
+    product: null,
+    error: null,
+    isLoading: false,
+    user: null,
+    users: []
   },
   getters: {
+    allProducts: (state) => state.products,
+    currentUser: (state) => state.user,
+    allUsers: (state) => state.users,
+    singleProduct: (state) => state.product,
   },
   mutations: {
+    SET_PRODUCTS(state, products) {
+      state.products = products;
+    },
+    SET_PRODUCT(state, product) {
+      state.product = product;
+    },
+    SET_LOADING(state, isLoading) {
+      state.isLoading = isLoading;
+    },
+    SET_ERROR(state, error) {
+      state.error = error;
+    },
+    SET_USER(state, user) {
+      state.user = user;
+    },
+    SET_USERS(state, users) {
+      state.users = users;
+    },
   },
   actions: {
+   async fetchProducts({ commit }) {
+    commit('SET_LOADING', true);
+    try {
+      const response = await axios.get(`${hostedData}product`);
+      if (response.status === 200) {
+        commit('SET_PRODUCTS', response.data.results);
+      }
+    } catch (error) {
+      commit('SET_ERROR', error.message);
+      console.error("Error fetching products:", error);
+    } finally {
+      commit('SET_LOADING', false);
+    }
   },
+
+  async fetchProduct({ commit }, productId) {
+    commit('SET_LOADING', true);
+    try {
+      const response = await axios.get(`${hostedData}product/${productId}`);
+      if (response.status === 200) {
+        commit('SET_PRODUCT', response.data.result);
+      }
+    } catch (error) {
+      commit('SET_ERROR', error.message);
+      console.error("Error fetching product:", error);
+    } finally {
+      commit('SET_LOADING', false);
+    }
+  },
+
+  async fetchUsers({ commit }) {
+    try {
+      const response = await axios.get(`${hostedData}user`);
+      if (response.status === 200) {
+        commit('SET_USERS', response.data.results);
+      }
+    } catch (error) {
+      commit('SET_ERROR', error.message);
+      console.error("Error fetching users:", error);
+    }
+  },
+
+  async fetchUser({ commit }, userId) {
+    try {
+      const response = await axios.get(`${hostedData}user/${userId}`);
+      if (response.status === 200) {
+        commit('SET_USER', response.data.result);
+      }
+    } catch (error) {
+      commit('SET_ERROR', error.message);
+      console.error("Error fetching user:", error);
+    }
+  },
+
+  async register({ commit }, payload) {
+    try {
+      const response = await axios.post(`${hostedData}user/register`, payload);
+      if (response.status === 200) {
+        commit('SET_USER', response.data.result);
+      }
+    } catch (error) {
+      commit('SET_ERROR', error.message);
+      console.error("Error registering user:", error);
+    }
+  },
+
+  async addProduct({ dispatch }, payload) {
+    try {
+      const response = await axios.post(`${hostedData}product/add`, payload);
+      if (response.status === 200) {
+        dispatch('fetchProducts');
+      }
+    } catch (error) {
+      console.error("Error adding product:", error);
+    }
+  },
+
+  async updateProduct({ dispatch }, payload) {
+    try {
+      const response = await axios.patch(`${hostedData}product/${payload.id}`, payload);
+      if (response.status === 200) {
+        dispatch('fetchProducts');
+      }
+    } catch (error) {
+      console.error("Error updating product:", error);
+    }
+  },
+
+  async deleteProduct({ dispatch }, productId) {
+    try {
+      const response = await axios.delete(`${hostedData}product/${productId}`);
+      if (response.status === 200) {
+        dispatch('fetchProducts');
+      }
+    } catch (error) {
+      console.error("Error deleting product:", error);
+    }
+  }
+},
   modules: {
   }
 })
--- Documents/AuraArtistry/aura-artistry/src/views/ContactView.vue
+++ Documents/AuraArtistry/aura-artistry/src/views/ContactView.vue
@@ -1,157 +1,160 @@
 <template>
-    <NavbarComp/>
-    <div class="contact-page">
-      <h2>Get in Touch</h2>
-      <p>
-        Want to learn more about AuraArtistry or book a makeup session with one of our artists? Fill out the form below and we'll get back to you soon!
-      </p>
-      <form @submit.prevent="submitForm">
-        <div class="form-group">
-          <label for="name">Name:</label>
-          <input type="text" id="name" v-model="name" @blur="validateName">
-          <div v-if="nameError" class="error">{{ nameError }}</div>
-        </div>
-        <div class="form-group">
-          <label for="email">Email:</label>
-          <input type="email" id="email" v-model="email" @blur="validateEmail">
-          <div v-if="emailError" class="error">{{ emailError }}</div>
-        </div>
-        <div class="form-group">
-          <label for="phone">Phone:</label>
-          <input type="tel" id="phone" v-model="phone" @blur="validatePhone">
-          <div v-if="phoneError" class="error">{{ phoneError }}</div>
-        </div>
-        <div class="form-group">
-          <label for="message">Message:</label>
-          <textarea id="message" v-model="message" @blur="validateMessage"></textarea>
-          <div v-if="messageError" class="error">{{ messageError }}</div>
-        </div>
-        <button type="submit">Send Message</button>
-      </form>
-    </div>
-    <FooterComp/>
-  </template>
-  
-  <script>
-  // // eslint-disable-next-line no-unused-vars
-import NavbarComp from '../components/NavBarComp.vue';
+  <NavbarComp/>
+  <div class="contact-page-wrapper">
+      <div class="contact-page">
+          <h2>Get in Touch</h2>
+          <p>
+              Want to learn more about AuraArtistry or book a makeup session with one of our artists? Fill out the form below and we'll get back to you soon!
+          </p>
+          <form @submit.prevent="submitForm">
+              <div class="form-group">
+                  <label for="name">Name:</label>
+                  <input type="text" id="name" v-model="name" @blur="validateName">
+                  <div v-if="nameError" class="error">{{ nameError }}</div>
+              </div>
+              <div class="form-group">
+                  <label for="email">Email:</label>
+                  <input type="email" id="email" v-model="email" @blur="validateEmail">
+                  <div v-if="emailError" class="error">{{ emailError }}</div>
+              </div>
+              <div class="form-group">
+                  <label for="message">Message:</label>
+                  <textarea id="message" v-model="message" @blur="validateMessage"></textarea>
+                  <div v-if="messageError" class="error">{{ messageError }}</div>
+              </div>
+              <button type="submit">Send Message</button>
+          </form>
+      </div>
+  </div>
+  <FooterComp/>
+</template>
 
-// // eslint-disable-next-line no-unused-vars
+<script>
+import NavbarComp from '../components/NavBarComp.vue'
 import FooterComp from '../components/FooterComp.vue'
 
-  export default {
-    data() {
+export default {
+  data() {
       return {
-        name: '',
-        email: '',
-        phone: '',
-        message: '',
-        nameError: '',
-        emailError: '',
-        phoneError: '',
-        messageError: ''
+          name: '',
+          email: '',
+          phone: '',
+          message: '',
+          nameError: '',
+          emailError: '',
+          messageError: ''
       }
-    },
-    methods: {
+  },
+  methods: {
       validateName() {
-        if (this.name.trim() === '') {
-          this.nameError = 'Please enter your name'
-        } else {
-          this.nameError = ''
-        }
+          if (this.name.trim() === '') {
+              this.nameError = 'Please enter your name'
+          } else {
+              this.nameError = ''
+          }
       },
       validateEmail() {
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-        if (!emailRegex.test(this.email)) {
-          this.emailError = 'Please enter a valid email address'
-        } else {
-          this.emailError = ''
-        }
-      },
-      validatePhone() {
-        const phoneRegex = /^\d{3}-\d{3}-\d{4}$/
-        if (!phoneRegex.test(this.phone)) {
-          this.phoneError = 'Please enter a valid phone number (XXX-XXX-XXXX)'
-        } else {
-          this.phoneError = ''
-        }
+          const atPosition = this.email.indexOf('@');
+          const dotPosition = this.email.lastIndexOf('.');
+
+          if (atPosition > 0 && dotPosition > atPosition + 1 && dotPosition < this.email.length - 1) {
+              this.emailError = '';
+          } else {
+              this.emailError = 'Please enter a valid email address';
+          }
       },
       validateMessage() {
-        if (this.message.trim() === '') {
-          this.messageError = 'Please enter a message'
-        } else {
-          this.messageError = ''
-        }
+          if (this.message.trim() === '') {
+              this.messageError = 'Please enter a message'
+          } else {
+              this.messageError = ''
+          }
       },
       submitForm() {
-        if (this.nameError || this.emailError || this.phoneError || this.messageError) {
-            alert('Please fix the errors before submitting the form')
-        } else {
-            const formData = new FormData()
-            formData.append('name', this.name)
-            formData.append('email', this.email)
-            formData.append('phone', this.phone)
-            formData.append('message', this.message)
+          if (this.nameError || this.emailError || this.phoneError || this.messageError) {
+              alert('Please fix the errors before submitting the form')
+          } else {
+              const formData = new FormData()
+              formData.append('name', this.name)
+              formData.append('email', this.email)
+              formData.append('phone', this.phone)
+              formData.append('message', this.message)
 
-            fetch('https://formspree.io/your-formspree-email', {
-            method: 'POST',
-            body: formData
-            })
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error(error))
-        }
-        }
-    },
-    components: {
-    NavbarComp,
-    FooterComp
-    }
+              fetch("https://formspree.io/f/xrgnrzgz", {
+                  method: 'POST',
+                  body: formData
+              })
+              .then(response => response.json())
+              .then(data => console.log(data))
+              .catch(error => console.error(error))
+          }
+      }
+  },
+  components: {
+      NavbarComp,
+      FooterComp
   }
-  </script>
-  
-  <style>
-  .contact-page {
-    max-width: 600px;
-    margin: 40px auto;
-    padding: 20px;
-    background-color: #f9f9f9;
-    border: 1px solid #ddd;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  }
-  
-  .form-group {
-    margin-bottom: 20px;
-  }
-  
-  label {
-    display: block;
-    margin-bottom: 10px;
-  }
-  
-  input, textarea {
-    width: 100%;
-    padding: 10px;
-    margin-bottom: 10px;
-    border: 1px solid #ccc;
-  }
-  
-  .error {
-    color: #f00;
-    font-size: 12px;
-    margin-bottom: 10px;
-  }
-  
-  button[type="submit"] {
-    background-color: #333;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-  
-  button[type="submit"]:hover {
-    background-color: #444;
-  }
-  </style>
+}
+</script>
+
+<style>
+.contact-page-wrapper {
+  display: flex;
+  justify-content: center;
+  max-width: auto;
+  height: 100vh;
+  padding: 20px;
+  background-image: url(https://tyra-parring.github.io/host-/image/pexels-mdsnmdsnmdsn-1831234.jpg);
+}
+
+.contact-page {
+  flex-basis: 60%;
+  padding: 20px;
+  background-color: #f9f9f9;
+  border: 1px solid #ddd;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+label {
+  display: block;
+  margin-bottom: 10px;
+}
+
+input, textarea {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+}
+
+.error {
+  color: #f00;
+  font-size: 12px;
+  margin-bottom: 10px;
+}
+
+input, textarea {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  height: 30px;
+}
+
+button[type="submit"] {
+  background-color: #333;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button[type="submit"]:hover {
+  background-color: #444;
+}
+</style>
