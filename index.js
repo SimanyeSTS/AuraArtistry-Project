b30848f77ae2874af45157f260876696967b7d82
SimanyeSTS <simanyesomdaka@gmail.com>
2024-08-22 14:44:30 +0200

    homepage color scheme fixed

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ import { productRouter } from './controller/ProductController.js';
 const app =  express()
 const port = process.env.PORT || 4000;
 
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(cors({
     origin: '*',
     credentials: true,
@@ -14,9 +18,6 @@ app.use(cors({
     allowedHeaders: '*',
     exposedHeaders: ['Authorization'],
 }))
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static('./static'))
 
