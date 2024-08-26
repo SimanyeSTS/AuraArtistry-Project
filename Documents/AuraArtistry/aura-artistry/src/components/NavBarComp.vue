9d03d977d930de6704e5ed71b1e58c612df9a4df
Tyra Parring <parringt@gmail.com>
2024-08-26 08:32:40 +0200

    fixed nav linking issues

--- Documents/AuraArtistry/aura-artistry/src/components/NavBarComp.vue
+++ Documents/AuraArtistry/aura-artistry/src/components/NavBarComp.vue
@@ -4,12 +4,12 @@
         <img src="https://tyra-parring.github.io/host-/image/AURA%20ARTTISRY.png" alt="AuraArtistry Logo" />
       </div>
       <ul class="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/products">Products</a></li>
-        <!-- <li><a href="product">Product</a></li> -->
-        <li><a href="/admin">Admin</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><RouterLink to="/">Home</RouterLink></li>
+      <li><RouterLink to="/about">About</RouterLink></li>
+      <li><RouterLink to="/products">Products</RouterLink></li>
+      <!-- <li><RouterLink to="/product">Product</RouterLink></li> -->
+      <li><RouterLink to="/admin">Admin</RouterLink></li>
+      <li><RouterLink to="/contact">Contact</RouterLink></li>
       </ul>
     </nav>
   </template>
